51a2bd99deeb9ea5813a6d9f511e5367783daba2
Pranav <pranavkoirala7@protonmail.com>
2023-01-09 23:13:51 -0800

    Fixed slow performance and fixed browser crash

--- scripts/algorithms/bfs.js
+++ scripts/algorithms/bfs.js
@@ -3,118 +3,92 @@ import { enableButtons } from "../index.js";
 export function bfs(grid, startNode, endNode, speed) {
   const visitedNodes = new Set();
   const queue = [startNode];
+  visitedNodes.add(startNode);
+
+  const previousNodes = new Map();
+  let reachedEndNode = false;
 
   const loop = () => {
-    if (queue.length > 0) {
+    if (!reachedEndNode) {
       const currentNode = queue.shift();
 
       if (currentNode === endNode) {
-        const shortestPath = getShortestPath(startNode, endNode, visitedNodes);
-        animateShortestPath(shortestPath, speed, startNode);
-        enableButtons();
-        return;
+        reachedEndNode = true;
       }
 
-      visitedNodes.add(currentNode);
+      for (const neighbor of getNeighboringNodes(currentNode)) {
+        if (!visitedNodes.has(neighbor)) {
+          visitedNodes.add(neighbor);
+          previousNodes.set(neighbor, currentNode);
+          queue.push(neighbor);
+        }
+      }
 
       if (!currentNode.classList.contains("shortest-path")) {
         currentNode.classList.add("visited-node", "animate");
       }
 
-      const neighbors = getNeighboringNodes(currentNode);
-      for (const neighbor of neighbors) {
-        queue.push(neighbor);
+      setTimeout(loop, speed);
+    } else {
+      const shortestPath = [];
+
+      let currentNode = endNode;
+
+      while (currentNode !== startNode) {
+        shortestPath.unshift(currentNode);
+        currentNode = previousNodes.get(currentNode);
+        if (currentNode === startNode) {
+          break;
+        }
       }
 
-      setTimeout(loop, speed);
+      let i = 0;
+      const animateShortestPath = () => {
+        startNode.classList.add("shortest-path", "animate");
+        if (i < shortestPath.length) {
+          shortestPath[i].classList.add("shortest-path", "animate");
+          shortestPath[i].classList.remove("visited-node");
+          i++;
+          setTimeout(animateShortestPath, speed + 50);
+        }
+      };
+
+      animateShortestPath();
+
+      enableButtons();
     }
   };
 
   loop();
-  function getNeighboringNodes(node) {
-    const neighboringNodes = [];
+}
 
-    const row = parseInt(node.dataset.row);
-    const col = parseInt(node.dataset.col);
+function getNeighboringNodes(node) {
+  const neighboringNodes = [];
 
-    if (col > 0) {
-      neighboringNodes.push(node.parentElement.children[col - 1]);
-    }
+  const row = parseInt(node.dataset.row);
+  const col = parseInt(node.dataset.col);
 
-    if (col < node.parentElement.children.length - 1) {
-      neighboringNodes.push(node.parentElement.children[col + 1]);
-    }
+  if (col > 0) {
+    neighboringNodes.push(node.parentElement.children[col - 1]);
+  }
 
-    if (row > 0) {
-      neighboringNodes.push(
-        node.parentElement.parentElement.children[row - 1].children[col]
-      );
-    }
+  if (col < node.parentElement.children.length - 1) {
+    neighboringNodes.push(node.parentElement.children[col + 1]);
+  }
 
-    if (row < node.parentElement.parentElement.children.length - 1) {
-      neighboringNodes.push(
-        node.parentElement.parentElement.children[row + 1].children[col]
-      );
-    }
-    return neighboringNodes.filter(
-      (neighbor) => !neighbor.classList.contains("wall")
+  if (row > 0) {
+    neighboringNodes.push(
+      node.parentElement.parentElement.children[row - 1].children[col]
     );
   }
 
-  function getShortestPath(startNode, endNode, visitedNodes) {
-    console.log("Entered getShortestPath");
-    const shortestPath = [];
-    let currentNode = endNode;
-    while (currentNode !== startNode) {
-      shortestPath.unshift(currentNode);
-      currentNode = getPreviousNode(currentNode, visitedNodes);
-      console.log(currentNode);
-    }
-    return shortestPath;
+  if (row < node.parentElement.parentElement.children.length - 1) {
+    neighboringNodes.push(
+      node.parentElement.parentElement.children[row + 1].children[col]
+    );
   }
 
-  function getPreviousNode(node, visitedNodes) {
-    const row = parseInt(node.dataset.row);
-    const col = parseInt(node.dataset.col);
-    if (col > 0 && visitedNodes.has(node.parentElement.children[col - 1])) {
-      return node.parentElement.children[col - 1];
-    }
-    if (
-      col < node.parentElement.children.length - 1 &&
-      visitedNodes.has(node.parentElement.children[col + 1])
-    ) {
-      return node.parentElement.children[col + 1];
-    }
-    if (
-      row > 0 &&
-      visitedNodes.has(
-        node.parentElement.parentElement.children[row - 1].children[col]
-      )
-    ) {
-      return node.parentElement.parentElement.children[row - 1].children[col];
-    }
-    if (
-      row < node.parentElement.parentElement.children.length - 1 &&
-      visitedNodes.has(
-        node.parentElement.parentElement.children[row + 1].children[col]
-      )
-    ) {
-      return node.parentElement.parentElement.children[row + 1].children[col];
-    }
-    return null;
-  }
-  function animateShortestPath(shortestPath, speed, startNode) {
-    let i = 0;
-    startNode.classList.add("shortest-path", "animate");
-    startNode.classList.remove("visited-node");
-    const animate = () => {
-      if (i < shortestPath.length) {
-        shortestPath[i].classList.add("shortest-path", "animate");
-        shortestPath[i].classList.remove("visited-node");
-        i++;
-        setTimeout(animate, speed + 50);
-      }
-    };
-    animate();
-  }
+  return neighboringNodes.filter(
+    (neighbor) => !neighbor.classList.contains("wall")
+  );
 }
